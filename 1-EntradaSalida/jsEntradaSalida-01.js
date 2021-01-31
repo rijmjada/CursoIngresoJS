2ee7973ec7e102a6f0cf910787c8b5d7ffcb04f3
Js <rijmjada@gmail.com>
2021-01-31 18:47:51 -0300

    subir

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,9 +3,7 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-let a;
-a = prompt ("Ingrese un dato");
-alert (" Usted ingreso :  "+a);
+	alert (" Esto funciona de maravilla")
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -2,7 +2,10 @@
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	alert("ok");
-}
 
+{
+	let a;
+a = prompt ("Ingrese un dato");
+alert (" Usted ingreso :  "+a);
+
+}
