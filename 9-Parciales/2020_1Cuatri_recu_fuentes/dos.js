f9aa02521a3379326c023902037c83a221a60005
Js <rijmjada@gmail.com>
2021-02-27 16:39:52 -0300

    subir

--- 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
@@ -11,7 +11,7 @@ a) El importe total a pagar , bruto sin descuento y...
 b) el importe total a pagar con descuento(solo si corresponde)
 d) Informar el tipo ("arena";"cal";"cemento") con mas cantidad de bolsas en el total de la compra.
 f) El tipo mas caro
-*/
+
 
 function mostrar(){
 
@@ -120,4 +120,131 @@ function mostrar(){
   }
 
 
-}//f
+}
+
+Realizar el algoritmo que permita ingresar los datos de una compra de productos de la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+al terminar la compra el cliente accede a un descuento segun las bolsas en total
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+
+d) Informar el tipo ("arena";"cal";"cemento") con mas cantidad de bolsas en el total de la compra.
+f) El tipo mas caro
+*/
+
+function  mostrar(){
+
+  let tipoProductoUsuario;
+  let cantidadUsuario;
+  let precioUsuario;
+  let consultaUsuario;
+  let acumuladorBolsas;
+  let acumuladorArena;
+  let acumuladorCal;
+  let acumuladorCemento;
+  let porcentajeDescuento;
+  let importeSubtotal;
+  let importeConDescuento;  
+  let descuentoAplicado;
+  let banderaComparador;
+  let nombreMasCaro;
+  let comparadorMasCaro;
+  let mayorCantidad;
+
+  
+  banderaComparador = 0;
+  descuentoAplicado = 0;
+  porcentajeDescuento = 0;
+  importeConDescuento = 0;
+  importeSubtotal = 0;
+  acumuladorArena = 0;
+  acumuladorCal = 0;
+  acumuladorCemento = 0;
+  acumuladorBolsas = 0;
+  consultaUsuario = "si";
+
+  while(consultaUsuario == "si"){
+
+    tipoProductoUsuario = prompt("Ingrese tipo de producto (cal, arena o cemento) : " );
+       while(!(tipoProductoUsuario == "cal" || tipoProductoUsuario == "arena" || tipoProductoUsuario == "cemento")){
+         tipoProductoUsuario = prompt("DATO INVALIDO. Ingrese tipo de producto (cal, arena o cemento) : " );
+      }
+
+    cantidadUsuario = prompt("Ingrese cantidad de bolsas : ");
+    cantidadUsuario = parseInt(cantidadUsuario);
+        while(isNaN(cantidadUsuario)== true ){
+          cantidadUsuario = prompt("DATO INVALIDO. Ingrese cantidad de bolsas : ");
+          cantidadUsuario = parseInt(cantidadUsuario);
+        }
+
+    precioUsuario = prompt("ingrese el precio : ");
+    precioUsuario = parseFloat(precioUsuario);
+      while(isNaN(precioUsuario)== true || precioUsuario < 1){
+        precioUsuario = prompt("DATO INVALIDO. ingrese el precio : ");
+        precioUsuario = parseFloat(precioUsuario);
+      } 
+      
+    switch(tipoProductoUsuario){
+
+        case "arena":
+          acumuladorArena = acumuladorArena + cantidadUsuario;
+          importeSubtotal = importeSubtotal + (precioUsuario * cantidadUsuario); 
+          break;
+        case "cal":
+          acumuladorCal = acumuladorCal + cantidadUsuario;
+          importeSubtotal = importeSubtotal + (precioUsuario * cantidadUsuario); 
+          break;
+        case "cemento":
+          acumuladorCemento = acumuladorCemento + cantidadUsuario;
+          importeSubtotal = importeSubtotal + (precioUsuario * cantidadUsuario); 
+          break;
+
+    }//switch  
+
+    if(banderaComparador == 0 ||comparadorMasCaro > precioUsuario){
+      comparadorMasCaro = precioUsuario;
+      nombreMasCaro = tipoProductoUsuario;
+      banderaComparador = 1;
+    }
+
+    ////////////// FIN VALIDACIONES ////////////////  
+    acumuladorBolsas = acumuladorBolsas + cantidadUsuario;
+    consultaUsuario = prompt("Ingrese ´si´ para continuar : ");
+
+  }//while principal
+
+  if(acumuladorBolsas > 30){
+      porcentajeDescuento = 25;
+    }
+  else{
+      if(acumuladorBolsas > 10){
+        porcentajeDescuento = 15;
+      }
+    
+  }
+
+  if(acumuladorArena > acumuladorCal && acumuladorArena > acumuladorCemento){
+     mayorCantidad = "Arena";
+   }else if(acumuladorCemento > acumuladorCal && acumuladorCemento > acumuladorArena){
+      mayorCantidad = "Cemento";
+    }
+    else{
+    mayorCantidad = "Cal";
+    }
+
+  descuentoAplicado = importeSubtotal * porcentajeDescuento / 100;
+  importeConDescuento = importeSubtotal - descuentoAplicado;
+
+  alert ("El importe sin descuento es : "+ importeSubtotal);
+  if(importeConDescuento != 0){
+      alert("El importe con descuento es : " + importeConDescuento);
+    }
+
+  alert ("El tipo mas caro es : "+ nombreMasCaro);
+
+
+}//fuction
--- 9-Parciales/2020_1Cuatri_recu_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/tres.js
@@ -11,43 +11,109 @@ e) El promedio de edad entre las mujeres casadas.*/
 function mostrar()
 {
 
-let nombreIngreso;
-let nacionalidadIngreso;
-let edadIngreso;
-let sexoIngreso;
-let estadoCivilIngreso;
-let temperaturaIngreso;
-let consultaUsuario;
-let nacionalidadTemperatura;
+	let nombreIngreso;
+	let nacionalidadIngreso;
+	let edadIngreso;
+	let sexoIngreso;
+	let estadoCivilIngreso;
+	let temperaturaIngreso;
+	let consultaIngreso;
+	let comparadorTemperatura;
+	let banderaNacionalidadTemperatura;
+	let nacionalidadMayorTemperatura;
+	let contadormayores17Solteros;
+	let contadorMujeresSolterasOViudas;
+	let contador60temp;
+	let acumuladorEdadMujeresCasadas;
+	let promedioMujeresCasadas;
+	let contadorMujeresCasadas;
 
-let banderaNacionalidadTemperatura;
+	promedioMujeresCasadas = 0;
+	acumuladorEdadMujeresCasadas = 0;
+	contadorMujeresCasadas = 0;
+	contador60temp = 0;
+	contadorMujeresSolterasOViudas = 0;
+	contadormayores17Solteros = 0;
+	banderaNacionalidadTemperatura = 0;
+	comparadorTemperatura = 0;
+	consultaIngreso = "si";
 
-banderaNacionalidadTemperatura = 0;
-
-consultaUsuario = "si";
-
-while(consultaUsuario == "si"){
+	while(consultaIngreso == "si"){
 
 	nombreIngreso = prompt("Ingrese nombre : ");
-	
-	nacionalidadIngreso = prompt("Ingrese nacionalidad : ");
+		while(isNaN(nombreIngreso)==false){
+			nombreIngreso = prompt("DATO INVALIDO. Ingrese nombre : ");
+		}
+
+	nacionalidadIngreso = prompt("Ingrese nacionalidad : ")
+		while(isNaN(nacionalidadIngreso)==false){
+			nacionalidadIngreso = prompt("DATO INVALIDO. Ingrese nacionalidad : ")
+		}
 
 	edadIngreso = prompt("Ingrese edad : ");
+		while(isNaN(edadIngreso)==true){
+			edadIngreso = prompt("DATO INVALIDO.Ingrese edad : ");
+		}	
 
-	sexoIngreso = prompt("Ingrese femenino o masculino : ");
+	sexoIngreso = prompt("Ingrese sexo (masculino o femenino) :");
+		while(isNaN(sexoIngreso)==false || sexoIngreso != "masculino" && sexoIngreso != "femenino"){
+			sexoIngreso = prompt("DATO INVALIDO. Ingrese sexo (masculino o femenino) :");
+		}	
 
 	estadoCivilIngreso = prompt("Ingrese estado civil (soltero, casado o viudo) : ");
+		while(isNaN(estadoCivilIngreso)==false || estadoCivilIngreso != "soltero" && estadoCivilIngreso != "casado" && estadoCivilIngreso != "viudo"){
+			estadoCivilIngreso = prompt("DATO INVALIDO. Ingrese estado civil (soltero, casado o viudo) : ");
+		}	
 
 	temperaturaIngreso = prompt("Ingrese temperatura : ");
+	temperaturaIngreso = parseInt(temperaturaIngreso);
+		while(isNaN(temperaturaIngreso)==true){
+			temperaturaIngreso = prompt("DATO INVALIDO. Ingrese temperatura : ");
+		temperaturaIngreso = parseInt(temperaturaIngreso);
+		}	
 
-	if(banderaNacionalidadTemperatura == 0 || temperaturaIngreso > nacionalidadTemperatura){
-		nacionalidadTemperatura = temperaturaIngreso;
+	///////////// FIN VALIDACIONES //////////////	
+
+	if(banderaNacionalidadTemperatura == 0 || temperaturaIngreso > comparadorTemperatura)
+		{
+		nacionalidadMayorTemperatura = nacionalidadIngreso;
 		banderaNacionalidadTemperatura = 1;
-	}else{}
+		}
 
-	
+	if(edadIngreso > 17 && estadoCivilIngreso == "soltero")
+		{
+		contadormayores17Solteros++;
+		}
 
+	if(sexoIngreso == "femenino" && (estadoCivilIngreso == "soltero" || estadoCivilIngreso == "viudo"))
+		{
+		contadorMujeresSolterasOViudas++;
+		}
 
-}//while
+	if(temperaturaIngreso > 38 && edadIngreso > 60)
+		{
+		contador60temp++;
+		}
+
+	if(sexoIngreso == "femenino" && estadoCivilIngreso == "casado")
+		{
+		contadorMujeresCasadas++;
+		acumuladorEdadMujeresCasadas = acumuladorEdadMujeresCasadas + edadIngreso;
+		}
+
+		
+	consultaIngreso = prompt("Ingrese ´si´ para continuar :");	
+
+	}// fin while
+
+	promedioMujeresCasadas = acumuladorEdadMujeresCasadas / contadorMujeresCasadas;
+
+	alert ("la Nacionalidad de la persona con mas temperatura es : "+ nacionalidadMayorTemperatura);
+	alert (" La cantidad de  mayores de edad( más de 17) que estan solteros es :" +contadormayores17Solteros);
+	alert ("La cantidad de Mujeres que hay solteras o viudas : "+contadorMujeresSolterasOViudas);
+	alert (" cuantas personas de la tercera edad( mas de 60 años) , tienen mas de 38 de temperatura : " +contador60temp);
+	if(promedioMujeresCasadas > 0){
+		alert ("El promedio de edad entre las mujeres casadas : " + promedioMujeresCasadas);
+		}
 
 }
